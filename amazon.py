1ae10dd48d21c083d74aaeb4e726ca9c44f843e4
Diego Chacón <dchacon130@gmail.com>
2022-07-21 10:01:17 -0500

    Last commit

--- amazon.py
+++ amazon.py
@@ -22,36 +22,32 @@ class Login(unittest.TestCase):
         cls.driver.maximize_window()
     
     def test_search_by_Alexa(self):
-        try:
-            self.driver.find_element(By.ID, "twotabsearchtextbox").send_keys('hiword')
-            self.driver.find_element(By.ID, "nav-search-submit-button").click()      
-            
-            element = WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located((By.LINK_TEXT, "2"))
-            )
-            element.click()
+        
+        self.driver.find_element(By.ID, "twotabsearchtextbox").send_keys('Alexa')
+        self.driver.find_element(By.ID, "nav-search-submit-button").click()
 
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//a[@class='s-pagination-item s-pagination-button'][contains(text(),'2')]").click() 
+
+        time.sleep(10)
+
+        entries = self.driver.find_elements(By.XPATH, "//span[@class = 'a-size-medium a-color-base a-text-normal']")
+
+        print(len(entries))
+        entries[2].click()
+
+
+        if self.driver.find_element(By.ID, "add-to-cart-button").is_displayed():
+            self.driver.find_element(By.ID, "add-to-cart-button").click()
             time.sleep(3)
-
-            entries = self.driver.find_elements(By.XPATH, "//span[@class = 'a-size-medium a-color-base a-text-normal']")
-
-            print(len(entries))
-            entries[2].click()
-
-
-            if self.driver.find_element(By.ID, "add-to-cart-button").is_displayed():
-                self.driver.find_element(By.ID, "add-to-cart-button").click()
-                time.sleep(3)
-                carNumber = self.driver.find_element(By.ID, "nav-cart-count").text
-                print(carNumber)
-            else:
-                noProduct = self.driver.find_element(By.XPATH, '//*[@id="outOfStock"]/div/div[1]/span[1]').text
-                print(noProduct)
-                self.assertEqual('No disponible por el momento.', noProduct, 'No hay cantidades de este producto por el momento.')
-                print('No hay cantidades de este producto por el momento.')
-        finally:
-            print("Se encontro un error en la automatización")
-            self.driver.quit()
+            carNumber = self.driver.find_element(By.ID, "nav-cart-count").text
+            print(carNumber)
+        else:
+            noProduct = self.driver.find_element(By.XPATH, '//*[@id="outOfStock"]/div/div[1]/span[1]').text
+            print(noProduct)
+            self.assertEqual('No disponible por el momento.', noProduct, 'No hay cantidades de este producto por el momento.')
+            print('No hay cantidades de este producto por el momento.')
+        
 
         
     @classmethod
